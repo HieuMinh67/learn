7fde187cf4b6498408a8d6046df78f55f551a365
Hieu Pham <minhhieu060799@gmail.com>
2023-09-11 17:08:29 +0700

    Add step function
    remove keys

---
+++ aws-dop-c02/step-func/orchestration/main.tf
@@ -0,0 +1,114 @@
+provider "aws" {}
+
+provider "archive" {}
+
+data "archive_file" "lambda" {
+  type = "zip"
+  output_path = "lambda_function_payload.zip"
+
+  source_file = "../functions/step-1/bin/bootstrap"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = data.archive_file.lambda.output_path
+  function_name = "step-1"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "bootstrap"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256(data.archive_file.lambda.output_path)
+
+  runtime = "provided.al2"
+  architectures = ["arm64"]
+}
+
+resource "aws_iam_role" "iam_for_sfn" {
+  name = "stepFunctionSampleStepFunctionExecutionIAM"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "states.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy_invoke_lambda" {
+  name        = "stepFunctionSampleLambdaFunctionInvocationPolicy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "lambda:InvokeFunction",
+                "lambda:InvokeAsync"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "iam_for_sfn_attach_policy_invoke_lambda" {
+  role       = aws_iam_role.iam_for_sfn.name
+  policy_arn = aws_iam_policy.policy_invoke_lambda.arn
+}
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "my-state-machine"
+  role_arn = aws_iam_role.iam_for_sfn.arn
+
+  definition = <<EOF
+{
+  "Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function",
+  "StartAt": "Hello",
+  "States": {
+    "Hello": {
+      "Type": "Task",
+      "Resource": "${aws_lambda_function.lambda.arn}",
+      "Next": "World"
+    },
+    "World": {
+      "Type": "Task",
+      "Resource": "${aws_lambda_function.lambda.arn}",
+      "End": true
+    }
+  }
+}
+EOF
+}
